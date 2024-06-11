af2173e0bf53131ac2867c10d35c28e81397f305
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-06-11 15:30:45 +0300

    update settings

--- plateforme/settings.py
+++ plateforme/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1', 'plateforme.onrender.com'], cast=Csv())
 
 
 # Application definition
